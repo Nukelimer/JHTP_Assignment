fa100cf3bf80d0477316c04096d1c3d6cfa1dc33
Nukelimer <codewithremi@gmail.com>
2025-10-04 22:43:37 +0100

    started chapter 5 playground

---
+++ src/main/java/Chapter_5_Exercises/Playground/AutoPolicy/AutoPolicyTest.java
@@ -0,0 +1,19 @@
+package Chapter_5_Exercises.Playground.AutoPolicy;
+
+public class AutoPolicyTest {
+
+    public static void main(String[] args) {
+        AutoPolicy autoPolicy = new AutoPolicy(123456, "Toyota Camry", "NY");
+
+        policyInNoFaultState(autoPolicy);
+
+    }
+    public static void policyInNoFaultState(AutoPolicy policy) {
+        System.out.println("The auto policy:");
+        System.out.printf(
+                "Account #: %d; Car: %s;%nState %s %s a no-fault state%n%n",
+                policy.getAccountNumber(), policy.getMakeAndModel(),
+                policy.getState(),
+                (policy.isNoFaultState() ? "is": "is not"));
+    }
+}
---
+++ src/main/java/Chapter_5_Exercises/Playground/Playground.java
@@ -0,0 +1,57 @@
+package Chapter_5_Exercises.Playground;
+
+public class Playground {
+
+    public static void main(String[] args) {
+
+//        for (int i = 1; i <= 10 ; ++i) {
+//            System.out.println(i);
+//        }
+//
+//        int j = 1;
+
+
+//        while (j <= 10){
+//            System.out.println(j);
+//            j++;
+//        }
+
+//           int total = 0;
+//        for (int i = 1; i <= 20 ; i++) {
+//
+//
+//           if (i % 2 == 0){
+//               total = total + i;
+//
+//           }
+//
+//        }
+
+//        for (int number = 2; number <= 20; number = number + 2) {
+//
+//            total = total + number;
+//        }
+//
+//        System.out.println(total);
+//
+//
+//        double principal = 1000.0;
+//        double rate = 0.05;
+//
+//        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+//
+//        for (int year = 1; year <= 10; ++year) {
+//
+//            double amount = principal * Math.pow(1.0 + rate, year);
+//
+//            System.out.printf("%4d%,20.2f%n", year, amount);
+//        }
+//
+//
+//
+//
+//
+
+
+    }
+}
