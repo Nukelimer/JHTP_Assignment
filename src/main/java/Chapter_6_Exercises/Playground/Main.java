3c6b508e681476b1744909a09ee63df0cdb7ff48
Nukelimer <codewithremi@gmail.com>
2025-10-11 20:21:45 +0100

    started chapter 6

---
+++ src/main/java/Chapter_6_Exercises/Playground/Main.java
@@ -0,0 +1,14 @@
+package Chapter_6_Exercises.Playground;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(900.00));
+        System.out.println(Math.ceil(-10.8));
+        System.out.println( MaximumFinder.maximum(32, 1,322));
+
+    }
+}
