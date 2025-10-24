2950dc527c2975ebf553ec88e9baf6ff23893372
Nukelimer <codewithremi@gmail.com>
2025-10-24 23:22:17 +0100

    started chapter 7 exercises

--- src/main/java/Chapter_7_Exercises/Exercises/Seven_dot_7/Main.java
+++ src/main/java/Chapter_7_Exercises/Exercises/Seven_dot_7/Main.java
@@ -7,13 +7,34 @@ public class Main {
 Determine whether each of the following is true or false. If false, explain why.
 a) To refer to a particular location or element within an array, we specify the name of the
 array and the order of the element in the array, assuming ordering starts at position 1.
-b) An array declaration initializes the elements in the array to the integer 0 by default.
+[FALSE, ARRAYS ARE ZERO BASED SO THE ORDER STARTS AT 0, BUT WE CAN ASSESS IT USING THE ARRAY'S NAME]
+
+b) An array declaration initializes the elements in the array to the integer 0 by default. [TRUE, BOOLEAN TO FALSE
+AND REFERENCE TYPES TO NULL]
+
 c) To indicate that 200 locations should be reserved for integer array p, you write the
-declaration int p[] = new int[200];
+declaration int p[] = new int[200];[CORRECT, A DISCREET LOCATION WILL BE CREATED IN
+ MEMORY FOR THE INTS AND DEFAULT
+TO 0]
+
+
 d) For an application that initializes the elements of a twenty-element integer array to zero,
-it is preferable to use some kind of loop.
+it is preferable to use some kind of loop. [TRUE, SOMETHING LIKE THIS FOR(INT I = 0; I < ARR.LENGTH; I++){
+ ARR[I] = 0;
+ }
+ ]
+
+
+
 e) To access all the elements in a two-dimensional array using a loop, the traversal across
-rows must be done in the outer loop and the traversal across columns in the inner loop*/
+rows must be done in the outer loop and the traversal across columns in the inner loop
+[TRUE, THE OUTER LOOP WILL LOOP THE ROW OR THE FIRST SECTION OF THE ARRAY, THEN THE INNER
+ARRAY WILL LOOP THE COLUMN.]
+
+
+*/
+
+
 
 
 
---
+++ src/main/java/Chapter_7_Exercises/Exercises/Seven_dot_8/Main.java
@@ -0,0 +1,129 @@
+package Chapter_7_Exercises.Exercises.Seven_dot_8;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+/*
+
+Write Java statements to accomplish each of the following tasks:
+a) Display the value of the tenth element of array r.
+b) Initialize each of the six elements of one-dimensional integer array g to -1.
+c) Find the maximum of the first one-hundred elements of floating-point array c.
+d) Copy a hundred-element array an into a hundred-element array b, but in reverse order.
+e) Compute the product of the third to the tenth elements, both inclusive, in a hundred-element integer array w.
+*/
+
+
+    public static void main(String[] args) {
+
+/*
+  a) Display the value of the tenth element of array r.
+ */
+ int[] tenArr ={1,2,3,4,5,12, 6,76,11,8,};
+//        System.out.println(tenArr[9]);
+
+        /*
+    b) Initialize each of the six elements of one-dimensional integer array g to -1.
+        */
+        int[] minusOneArr = new int[6];
+        Arrays.fill(minusOneArr, -1);
+
+//        for (int i = 0; i < minusOneArr.length; i++) {
+//            minusOneArr[i] = -1;
+//        }
+
+//        System.out.println(Arrays.toString(minusOneArr));
+
+    /*
+    c) Find the maximum of the first one-hundred elements of floating-point array c.
+    */
+
+        float[] hundredArr = new float[100];
+
+
+        Random random = new Random();
+        float x = random.nextFloat() * 10000;
+//        for (int i = 0; i < hundredArr.length ; i++) {
+//            float x = random.nextFloat(10000);
+//            hundredArr[i] = x;
+//
+//        }
+
+        Arrays.fill(hundredArr, x);
+
+        System.out.println(Arrays.toString(hundredArr));
+        float largest = hundredArr[3];
+        for (int i = 0; i < hundredArr.length; i++) {
+
+            if(hundredArr[i] > largest){
+                largest = hundredArr[i];
+
+            }
+        }
+
+        for (int i = hundredArr.length; i > 0 ; i--) {
+
+        }
+
+//        System.out.println(largest);
+
+
+
+/*
+ d) Copy a hundred-element array an into a hundred-element array b, but in reverse order.
+ */
+
+
+        int a[] = {4, 1,2,3};
+        int b[] = new  int[4];
+
+        for (int i =  0 ; i < a.length ; i++) {
+            b[i] = a[a.length - 1 - i];
+
+        }
+
+//        System.out.println(Arrays.toString(a));
+//        System.out.println(Arrays.toString(b));
+
+
+        /*
+
+e) Compute the product of the third to the tenth elements, both inclusive, in a hundred-element integer array w.
+
+ */
+
+        int[] w = {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+                61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+                71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+                81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+                91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+        };
+
+        int product = 1;
+
+        for (int i = 2; i <= 9; i++) {
+
+
+                product *= w[i];
+
+        }
+
+        System.out.println(product);
+
+
+
+
+    }
+
+
+
+
+}
