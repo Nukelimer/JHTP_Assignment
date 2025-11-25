0ced6e3f9c6e14d146225f4757479336aa0b2274
Nukelimer <codewithremi@gmail.com>
2025-11-25 19:43:25 +0100

    reading chapter 10.

---
+++ src/main/java/Chapter_10_Exercises/Playground/Ten_dot_1/BasePlusCommissionEmployee.java
@@ -0,0 +1,39 @@
+package Chapter_10_Exercises.Playground.Ten_dot_1;
+
+public class BasePlusCommissionEmployee  extends CommissionEmployee {
+    private double baseSalary;
+
+    public BasePlusCommissionEmployee(String firstName, String lastName,
+                                      String socialSecurityNumber, double grossSales,
+                                      double commissionRate) {
+        super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+
+
+        if (baseSalary < 0.0) {
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        }
+        this.baseSalary = baseSalary;
+
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary < 0.0) {
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {return baseSalary;}
+
+    @Override
+    public double earnings() {
+     return getBaseSalary() + super.earnings();
+    }
+
+    @Override
+    public String toString() {
+// not allowed: attempts to access private superclass members
+        return String.format("%s %s%n%s: %.2f", "base-salaried",
+                super.toString(), "base salary", getBaseSalary());
+    }
+}
